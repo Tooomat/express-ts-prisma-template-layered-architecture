533820efc03c151fdcc6d7f4a81274c9798bb95d
Tooomat <ardijr62@gmail.com>
2025-12-31 20:15:19 +0700

    add: added connect to postgre

--- src/application/database.ts
+++ src/application/database.ts
@@ -1,23 +1,27 @@
 import * as env from "../config/env"
-// import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaMariaDb } from '@prisma/adapter-mariadb'
+import { PrismaPg } from '@prisma/adapter-pg'
+// import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import { PrismaClient } from '../generated/prisma/client'
 import { logger } from "./logging";
 
 const isDev = env.config.NODE_ENV === "development";
+const connectionString = `${env.config.DATABASE_URL}`
 
-const adapter = new PrismaMariaDb({ 
-  host: env.config.DB_HOST,
-  port: env.config.DB_PORT,
-  user: env.config.DB_USER,
-  password: env.config.DB_PASSWORD,
-  database: env.config.DB_NAME,
+// use this if database using MYSQL
+// const adapter = new PrismaMariaDb({ 
+//   host: env.config.DB_HOST,
+//   port: env.config.DB_PORT,
+//   user: env.config.DB_USER,
+//   password: env.config.DB_PASSWORD,
+//   database: env.config.DB_NAME,
 
-  // Match Prisma ORM v6 defaults:
-  connectTimeout: 5_000, // v6 connect_timeout was 5s
-  idleTimeout: 300,      // v6 max_idle_connection_lifetime was 300s
+// })
 
+// use this if database using POSTGRES
+const adapter = new PrismaPg({ 
+  connectionString 
 })
+
 export const prismaClient = new PrismaClient({ 
     adapter,
     log: [ //apa saja yang dilog
